78d82cff39f90f618459b22c40c2ddc9b73d3d25
Neil Dahiya <neildahiya1@gmail.com>
2019-10-13 16:27:51 +0530

    Add files via upload
    An old project made using simple HTML,CSS and Javascript

---
+++ Todo List/assets/js/todos.js
@@ -0,0 +1,26 @@
+//Check off todos by clicking
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("completed");
+});
+
+
+$("ul").on("click", "span", function(event){
+	 $(this).parent().fadeOut(function(){
+	 	$(this).remove();
+	 });
+	 event.stopPropagation();
+});
+
+
+$("input[type = 'text']").keypress(function(event){
+	if (event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span> <i class='far fa-trash-alt'></i> </span>" + todoText + "</li>")
+	}
+});
+
+$(".fa-plus-square").click(function()
+	{
+		$("input[type='text']").fadeToggle();
+	});
